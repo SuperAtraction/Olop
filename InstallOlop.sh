ca01b23522d01446fde33ffe9cfefb07ae0d22ce
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2023-06-03 13:11:52 +0200

    Ajout de l'installer

---
+++ InstallOlop.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# URL du fichier ZIP à télécharger
+url="https://github.com/SuperAtraction/Olop/raw/main/Olop"
+
+# Chemin de destination pour le téléchargement et l'extraction
+destination="/opt/Olop/"
+
+# Chemin vers l'exécutable ou la bibliothèque Qt à analyser
+executable=$destination"Olop"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Exécuction en root"
+  pkexec $0
+  echo "Olop et ses dépendances ont étés installés avec succés. Olop va se lancer automatiquement"
+  $executable
+  exit
+fi
+
+mkdir $destination
+
+rm -f $destination"Olop"
+
+# Téléchargement du fichier
+wget --no-check-certificate -P $destination $url
+
+# Analyse des dépendances avec ldd
+dependencies=$(ldd $executable | awk '{if ($3 == "not") print $1}')
+
+# Installation des bibliothèques manquantes avec le gestionnaire de paquets approprié
+if command -v apt-get >/dev/null 2>&1; then
+    # Utilisation du gestionnaire de paquets apt
+    sudo apt-get update
+    for dependency in $dependencies; do
+        package=$(dpkg -S $dependency | cut -d: -f1)
+        if [ -n "$package" ]; then
+            sudo apt-get install --reinstall -y $package
+        fi
+    done
+elif command -v dnf >/dev/null 2>&1; then
+    # Utilisation du gestionnaire de paquets DNF (Fedora, CentOS, etc.)
+    sudo dnf check-update
+    for dependency in $dependencies; do
+        package=$(rpm -qf $dependency)
+        if [ -n "$package" ]; then
+            sudo dnf reinstall -y $package
+        fi
+    done
+elif command -v pacman >/dev/null 2>&1; then
+    # Utilisation du gestionnaire de paquets pacman (Arch Linux, Manjaro, etc.)
+    sudo pacman -Syu --needed
+    for dependency in $dependencies; do
+        package=$(pacman -Qo $dependency | cut -d' ' -f5)
+        if [ -n "$package" ]; then
+            sudo pacman -S --needed $package
+        fi
+    done
+else
+    echo "Gestionnaire de paquets non pris en charge"
+    exit 1
+fi
+
+chmod +x $executable
+
+exit
